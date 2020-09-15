243daece025644195344e16711c45b37307178eb
Bart Dority <b0rgBart3@gmail.com>
2020-09-15 08:36:36 -0700

    creating steady git flow

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php 
 session_start();
 include_once "config.php";
-echo "<div class='debuggingBox'>";
+//echo "<div class='debuggingBox'>";
 include_once "classes/artworks.php";
 include_once "scripts/load.php"; 
 include_once "scripts/in_memory_functions.php"; 
-
 load_full_works();
 $db = $_SESSION['artwork_db'];
 $images = load_images($db, 'current');
@@ -14,21 +13,18 @@ $works = $_SESSION['works'];
 $collections = splitIntoCategories();
 $_SESSION['collections'] = $collections;
 $mostCurrentId = get_most_current_id();
-echo "</div>"; // end of debugging box
+//echo "</div>"; // end of debugging box
 ?>
 <!DOCTYPE html>
-<meta charset = "UTF-8">
- <!-- name="viewport" content="initial-scale=1.0, user-scalable=1"> -->
-
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="og:title" content="Jerry Frost Artist">
 <meta property="og:type" content="website">
 <meta property="og:description" content="San Francisco Painter">
 <meta property="og:image" content="http://jerryfrost.com/interface/jerry_og2.jpg">
 <meta property="og:url" content="http://jerryfrost.com/">
 <meta property="fb:app_id" name="fb_app_id" content="429912884431270"/>
-
-<html lang="en">
+<html>
 <?php 
 include 'parts/head.php';
 ?>
