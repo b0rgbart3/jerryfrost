08577f015e835ced88915445bcacad5e013569d3
Bart <b0rgbart3@gmail.com>
2026-08-08 15:21:44 -0700

    clean up the scripts

--- scripts/not_found.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h1>Warning:</h1>";
-echo "That page was not found.<br>";
-echo "<a href='/index.php'>Click here to go to the homepage.</a>";
-
-?>
--- scripts/notok.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "You decided not to remove the painting.<br>";
-echo "Click <a href='review.php'>here</a> to go back to the admin page.";
-?>
